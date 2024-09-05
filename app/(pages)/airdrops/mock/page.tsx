424ce65a03b5654c68d1feb677dfa6bc392f3c30
takadr <t4k4.cr@gmail.com>
2024-09-05 13:04:29 +0900

    add component boilerplates

--- app/(pages)/airdrops/mock/page.tsx
+++ app/(pages)/airdrops/mock/page.tsx
@@ -1,23 +1,13 @@
 "use client";
-import styles from "./page.module.css";
 import { Center, Container, Heading, Spinner } from "@chakra-ui/react";
 import { useRequireAccount } from "@/app/hooks/common/useRequireAccount";
-import Test from "./test";
 
-export default function Dashboard() {
+export default function Airdrop() {
   const { address, isConnecting, isReconnecting } = useRequireAccount();
-
-  if (!address)
-    return (
-      <Center>
-        <Spinner />
-      </Center>
-    );
 
   return (
     <Container>
-      <Heading>Dashboard</Heading>
-      {/* <Test /> */}
+      <Heading>Airdrop</Heading>
     </Container>
   );
 }
---
+++ app/components/airdrops/OwnerMenu.tsx
@@ -0,0 +1,5 @@
+"use client";
+import {} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
+
+export default function OwnerMenu() {}
