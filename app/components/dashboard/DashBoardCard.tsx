6195728bc8446a2e4213a6a1ba93aca53aa32e9c
yosuke <yosuke2002jp@yahoo.co.jp>
2024-11-16 15:29:20 +0900

    add use client

--- app/components/dashboard/DashBoardCard.tsx
+++ app/components/dashboard/DashBoardCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Box, Avatar, Card, CardBody, Flex, Text } from "@chakra-ui/react";
 import { CheckCircleIcon } from "@chakra-ui/icons";
 import { Row, useIsMobile } from "@/app/lib/chakra/chakraUtils";
