d4837bd062e7201ee180f208e03407475fcb2309
yosuke <yosuke2002jp@yahoo.co.jp>
2024-12-08 14:01:47 +0900

    rename Airdrop to AirdropPage component

--- app/(pages)/airdrops/mock/page.tsx
+++ app/(pages)/airdrops/mock/page.tsx
@@ -2,9 +2,51 @@
 import { Center, Container, Heading, Spinner, VStack, Box } from "@chakra-ui/react";
 import { useRequireAccount } from "@/app/hooks/common/useRequireAccount";
 import AirdropDetails from "@/app/components/airdrops/cards/AirdropDetails";
+import { useIsMounted } from "@/app/hooks/common/useIsMounted";
+import { useTranslation } from "react-i18next";
+import { TemplateType, Airdrop } from "@/app/interfaces/dashboard";
 
-export default function Airdrop() {
+export default function AirdropPage() {
   const { address, isConnecting, isReconnecting } = useRequireAccount();
+  const isMounted = useIsMounted();
+  const { t } = useTranslation();
+  const airdrops: Airdrop[] = [
+    {
+      id: "asdf",
+      title: "Vesting",
+      templateName: TemplateType.LINEAR_VESTING,
+      owner: "0x",
+      tokenAddress: "0x",
+      createdAt: 1731897560,
+      merkleTreeUploadedAt: 1731897560,
+      contractAddress: "0x",
+      totalAirdropAmount: BigInt(0.005 * 1e18),
+      eligibleUsersNum: 1000,
+      claimedUsersNum: 40,
+      contractDeployedAt: 1731897560,
+      vestingEndsAt: 1731897560,
+    },
+    {
+      id: "zxcv",
+      title: "Standard",
+      templateName: TemplateType.STANDARD,
+      owner: "0x",
+      tokenAddress: "0x",
+      createdAt: 1731897560,
+      merkleTreeUploadedAt: undefined,
+      contractAddress: undefined,
+      totalAirdropAmount: BigInt(0.005 * 1e18),
+      eligibleUsersNum: 1000,
+      claimedUsersNum: 40,
+      contractDeployedAt: 1731897560,
+    },
+  ];
+  if (!isMounted || !address)
+    return (
+      <Center>
+        <Spinner />
+      </Center>
+    );
 
   return (
     <Container maxW={"container.xl"} mb={4}>
