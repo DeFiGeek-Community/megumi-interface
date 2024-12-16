3873c8c9d258fcdb61f4fa914a81a87ce819ab2a
takadr <t4k4.cr@gmail.com>
2024-12-16 12:13:25 +0900

    fix typo in the config file

--- app/lib/aws/index.ts
+++ app/lib/aws/index.ts
@@ -3,8 +3,8 @@ import { S3ClientConfig, S3Client } from "@aws-sdk/client-s3";
 export const s3Config: S3ClientConfig = {
   region: process.env.AWS_REGION,
     credentials:{
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
     }
 }
 
