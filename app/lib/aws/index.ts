2b5eb8dc494c0c3693d9e30db6dcfdf75cd516b3
takadr <t4k4.cr@gmail.com>
2024-12-20 11:39:47 +0900

    add S3 endpoint setting and update .env.sample

--- app/lib/aws/index.ts
+++ app/lib/aws/index.ts
@@ -2,10 +2,12 @@ import { S3ClientConfig, S3Client } from "@aws-sdk/client-s3";
 
 export const s3Config: S3ClientConfig = {
   region: process.env.AWS_REGION,
-    credentials:{
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-}
+  forcePathStyle: true,
+  endpoint: process.env.AWS_S3_ENDPOINT,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+};
 
-export const awsClient = new S3Client(s3Config)
+export const awsClient = new S3Client(s3Config);
