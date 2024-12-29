4508c00afc1853c0f4e6d9092bd9f1bf9e8e07bf
takadr <t4k4.cr@gmail.com>
2024-12-29 12:47:59 +0900

    remove lambda settings from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,24 @@ until curl -s http://localstack:4566 > /dev/null; do
 done
 echo "LocalStack is ready!"
 
+# # Global install
+# npm install -g @aws-amplify/cli -y
+
 # Create the S3 bucket
 /root/.local/pipx/venvs/awscli-local/bin/awslocal --endpoint-url=http://localstack:4566 s3 mb s3://localstack-bucket
 
 # Create lambda functions on localstack --------------->
 # Refs: https://docs.localstack.cloud/user-guide/aws/lambda/
 
-cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree/src
-# Install dependencies
-npm i -y
-# Bundle TypeScript code using esbuild
-npx esbuild ../ts/index.ts --bundle --platform=node --target=node18 --outfile=index.js
-# Zip the output file
-zip function.zip index.js
+# cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree/src
+# # Install dependencies
+# npm i -y
+# # Bundle TypeScript code using esbuild
+# npx esbuild ../ts/index.ts --bundle --platform=node --target=node18 --outfile=index.js
+# # Zip the output file
+# zip function.zip index.js
 
-# cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree && npx tsc -p ./tsconfig.json && cd ./src && zip function.zip index.js && cd -
+cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree && npx tsc -p ./tsconfig.json && cd ./src && zip function.zip index.js && cd -
 /root/.local/pipx/venvs/awscli-local/bin/awslocal lambda create-function \
     --endpoint-url=http://localstack:4566 \
     --function-name watchMegumiContractDeploymentStatusAndInsertMerkletree \
