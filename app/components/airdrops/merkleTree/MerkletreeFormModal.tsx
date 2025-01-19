ea91cdbdf69701deac818e86bea1693ce213ead9
takadr <t4k4.cr@gmail.com>
2025-01-19 18:30:00 +0900

    add merkle tree upload feature

---
+++ app/components/airdrops/merkleTree/MerkletreeFormModal.tsx
@@ -0,0 +1,184 @@
+"use client";
+import { useRef, useState } from "react";
+import { useFormik } from "formik";
+import { useSession } from "next-auth/react";
+import { useTranslation } from "react-i18next";
+import {
+  Button,
+  HStack,
+  chakra,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  FormErrorMessage,
+  FormControl,
+  useToast,
+  Input,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tab,
+  InputGroup,
+  InputLeftElement,
+  Icon,
+} from "@chakra-ui/react";
+import { FiFile } from "react-icons/fi";
+import { useRequireAccount } from "@/app/hooks/common/useRequireAccount";
+import { useUploadMerkletree } from "@/app/hooks/airdrops/useUploadMerkletree";
+
+type MerkletreeFormModalProps = {
+  chainId: number;
+  airdropId: string;
+  ownerAddress: `0x${string}`;
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+};
+
+type MerkletreeFileFormValues = {
+  fileinput?: string;
+};
+export default function MerkletreeFormModal({
+  chainId,
+  airdropId,
+  ownerAddress,
+  isOpen,
+  onOpen,
+  onClose,
+}: MerkletreeFormModalProps) {
+  const {
+    address,
+    isConnecting,
+    isReconnecting,
+    isConnected: isConnectedRaw,
+  } = useRequireAccount();
+  const { data: session } = useSession();
+  const { t } = useTranslation();
+  const toast = useToast({ position: "top-right", isClosable: true });
+  const { upload, loading, error } = useUploadMerkletree(chainId, airdropId);
+
+  const handleSubmit = async (data: MerkletreeFileFormValues) => {
+    formikProps.validateForm();
+    if (!file) return;
+    await upload(file, {
+      onSuccess: () => {
+        toast({ title: "Uplaod succeeded", status: "success" });
+        onClose();
+      },
+      onError: (error) => {
+        toast({ title: error, status: "error" });
+      },
+    });
+  };
+
+  const validate = (value: MerkletreeFileFormValues) => {
+    const errors: any = {};
+
+    if (!file) {
+      errors["file"] = "File is required";
+    }
+    return errors;
+  };
+  const initialValues: MerkletreeFileFormValues = {};
+  const formikProps = useFormik<MerkletreeFileFormValues>({
+    enableReinitialize: true,
+    validateOnChange: true,
+    initialValues,
+    onSubmit: handleSubmit,
+    validate: (value: MerkletreeFileFormValues) => validate(value),
+  });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [file, setFile] = useState<File | null>(null);
+
+  return (
+    <>
+      <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+        closeOnOverlayClick={false}
+        blockScrollOnMount={false}
+        isCentered={true}
+        size={"lg"}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{t("airdrop.merkletreeForm.registerContract")}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Tabs>
+              <TabList>
+                <Tab>リストファイルをアップロード</Tab>
+                <Tab isDisabled>マニュアル入力</Tab>
+                <Tab isDisabled>スナップショットから作成</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <form onSubmit={formikProps.handleSubmit}>
+                    <HStack spacing={8} alignItems={"start"}>
+                      <chakra.div w={"full"}>
+                        <FormControl isInvalid={!file && formikProps.submitCount > 0}>
+                          <p>エアドロップリストファイルをアップロードしてください</p>
+                          <InputGroup>
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<Icon as={FiFile} />}
+                            />
+                            <input
+                              type="file"
+                              accept={"application/json"}
+                              ref={inputRef}
+                              onChange={(e) => {
+                                e.target.files && setFile(e.target.files[0]);
+                                setTimeout(() => {
+                                  formikProps.validateForm();
+                                }, 10);
+                              }}
+                              style={{ display: "none" }}
+                            ></input>
+                            <Input
+                              placeholder={"Airdrop list file"}
+                              onClick={() => inputRef.current?.click()}
+                              onChange={(e) => {}}
+                              value={file ? file.name : ""}
+                              cursor={"pointer"}
+                            />
+                          </InputGroup>
+                          <FormErrorMessage>{"File is required"}</FormErrorMessage>
+                        </FormControl>
+                      </chakra.div>
+                    </HStack>
+
+                    <>
+                      <Button
+                        mt={4}
+                        w={"full"}
+                        variant="solid"
+                        colorScheme="green"
+                        type="submit"
+                        isLoading={loading}
+                        disabled={!formikProps.isValid}
+                      >
+                        {t("airdrop.contractForm.deploy")}
+                      </Button>
+                    </>
+                  </form>
+                </TabPanel>
+                <TabPanel>
+                  <p>TODO</p>
+                </TabPanel>
+                <TabPanel>
+                  <p>TODO</p>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
---
+++ app/hooks/airdrops/useUploadMerkletree.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { API_BASE_URL } from "@/app/lib/constants";
+import { getErrorMessage } from "@/app/utils/shared";
+
+const API_URL = `${API_BASE_URL}/airdrops`;
+
+type UploadCallbacks = {
+  onSuccess?: () => void;
+  onError?: (error: string) => void;
+};
+export const useUploadMerkletree = (chainId: number, airdropId: string) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const upload = async (file: File, { onSuccess, onError }: UploadCallbacks) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    setLoading(true);
+    try {
+      const res = await fetch(`${API_URL}/${chainId}/${airdropId}`, {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        setError(data.error);
+        onError?.(data.error);
+      } else {
+        onSuccess?.();
+      }
+      setLoading(false);
+    } catch (error: unknown) {
+      setLoading(false);
+      onError?.(getErrorMessage(error));
+    }
+  };
+  return { upload, loading, error };
+};
