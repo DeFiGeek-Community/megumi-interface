471c4dd44c63598f9b908cdf5788dc25e8d61536
takadr <t4k4.cr@gmail.com>
2025-01-23 14:19:11 +0900

    remove unused lambda settings in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,29 +12,5 @@ echo "LocalStack is ready!"
 # Create the S3 bucket
 /root/.local/pipx/venvs/awscli-local/bin/awslocal --endpoint-url=http://localstack:4566 s3 mb s3://localstack-bucket
 
-# Create lambda functions on localstack --------------->
-# Refs: https://docs.localstack.cloud/user-guide/aws/lambda/
-
-# cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree/src
-# # Install dependencies
-# npm i -y
-# # Bundle TypeScript code using esbuild
-# npx esbuild ../ts/index.ts --bundle --platform=node --target=node18 --outfile=index.js
-# # Zip the output file
-# zip function.zip index.js
-
-cd /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree && npx tsc -p ./tsconfig.json && cd ./src && zip function.zip index.js && cd -
-/root/.local/pipx/venvs/awscli-local/bin/awslocal lambda create-function \
-    --endpoint-url=http://localstack:4566 \
-    --function-name watchMegumiContractDeploymentStatusAndInsertMerkletree \
-    --runtime nodejs18.x \
-    --handler index.handler \
-    --zip-file fileb://function.zip \
-    --role arn:aws:iam::000000000000:role/lambda-role
-
-# npx amplify mock function --name watchMegumiContractDeploymentStatusAndInsertMerkletree --event /app/amplify/backend/function/watchMegumiContractDeploymentStatusAndInsertMerkletree/src/event.json
-# <---------------
-
-
 # Run the app
 exec "$@"
